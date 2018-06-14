c91b6133a3c4ca92ddc5d79b027dee92e0b9fb84
Othoniel Virgen <ov7664@gmail.com>
2018-06-13 22:40:29 -0500

    Works but is a bit cluncky...reset method doesn't work properly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,16 @@
 // Game's variables
 
 var wins = 0;
+var W = document.getElementById("number-of-wins");
+W.innerHTML = wins;
 
 var losses = 0;
+var L =document.getElementById("number-of-losses");
+L.innerHTML = losses;
 
 var guessesLeft = 9;
+var GL = document.getElementById("guesses-left");
+GL.innerHTML = guessesLeft
 
 // Guesses so far is an array because there are multipe varying letters every game
 
@@ -14,18 +20,18 @@ var guessesSoFar = [];
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-
 // Function that initiates the game
 
 document.onkeyup = function(event) {
-    // Gets the keys pressed by the user and uses the push() method to 
+    // Gets the keys pressed by the user and uses the push() method to
     var userGuess = event.key;
     guessesSoFar.push(userGuess);
-    // Answer is determined
     var letterAnswer = letters[Math.floor(Math.random() * letters.length)];
+    var GSF = document.getElementById("guesses-so-far");
+    GSF.innerHTML = guessesSoFar;
 
-// Reset Function
-    var reset = function() {
+    // Reset Function
+        var reset = function () {
         guessesLeft = 9;
         guessesSoFar = [];
     }
@@ -33,16 +39,21 @@ document.onkeyup = function(event) {
 // Win and loss conditions
     if (userGuess === letterAnswer) {
         wins++;
-        guessesLeft = 9;
+        reset();
+        var W = document.getElementById("number-of-wins");
+        W.innerHTML = wins
     }
 
-    else {
+    else if (userGuess !== letterAnswer) {
         guessesLeft--
+        var GL = document.getElementById("guesses-left");
+        GL.innerHTML = guessesLeft
     }
 
     if (guessesLeft === 0) {
         losses++;
-        guessesLeft = 9;
         reset();
+        var L = document.getElementById("number-of-losses");
+        L.innerHTML = losses
     }
 }
